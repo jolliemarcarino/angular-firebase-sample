b0c698147224329fa84ac8183a4cf6a91e0d1731
Jollie Mar Carino Gutang <jolliemar@fastjapan.com>
2018-02-04 21:05:19 +0900

    Angular base

---
+++ firebase-angular-chat-mar/angular5-firebase-chat/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+	production: false
+	, firebase : {
+		apiKey: "AIzaSyBG2wM1DgEeGAcGGyRU8PbT1LEhRER4Nso",
+		authDomain: "systemchat-28f9a.firebaseapp.com",
+		databaseURL: "https://systemchat-28f9a.firebaseio.com",
+		projectId: "systemchat-28f9a",
+		storageBucket: "",
+		messagingSenderId: "883477545400"
+	}
+};
